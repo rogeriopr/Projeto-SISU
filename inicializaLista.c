8d5ef0393daf3dc65befa572705ad19a95e5d170
caioasilva <caiosilva96@hotmail.com>
2015-06-29 20:47:30 -0300

    correção

--- inicializaLista.c
+++ inicializaLista.c
@@ -1,41 +1,43 @@
-int inicializaLista(tMatch match[], int nSelecionados[MAX_CURSOS][N_GRUPOS],tCandidato candidato[],int nCandidatos){
-    int i, j; //variaveis locais contadoras de laco
+int inicializaLista(tMatch match[], int nSelecionados[MAX_CURSOS][5],tCandidato candidato[],int nCandidatos,int nCursos)
+{
+    int i,j; //contadores de laço
     // inializacao do vetor lista com cada candidato tendo a variavel inicializada
-    
-    for(i=0; i<MAX_CANDIDATOS; i++){
-        lista[i].media = 0;
-    }       
-            
-    for(i=0; i<nCursos; i++){
-            for(j=0, j<N_CURSOS; j++){
-                nSelecionados[i][j]=0;
-            }
-        }
-        
-    j=0; // reaproveitamento do contador
-    
-    for(i=0; i<nCandidatos; i++){
-        // preenchimento da lista de candidatos
-        lista[j].idCandidato = i;
-        lista[j].idCurso=candidato[i].opcao1.idCurso;
-        lista[j].media=candidato[i].opcao1.media;
-        lista[j].opcao1=0;
-        lista[j].grupo=candidato[i].opcao.grupo;
-        lista[j].classificado=0;
-        j++;
-        printf("%s - Opcao 1", candidato[i].nome);
-        
-        if(candidato[i].opcao2.idCurso!=-1){
-        lista[j].idCandidato=i;
-        lista[j].idCurso=candidato[i].opcao1.idCurso;
-        lista[j].media=candidato[i].opcao1.media;
-        lista[j].opcao1=0;
-        lista[j].grupo=candidato[i].opcao.grupo;
-        lista[j].classificado=0;
-        j++;
-        printf("/Opcao 2");
-        }      
+    printf("Inicializando lista de candidatos:\n");
+    for(i=0;i<MAX_CANDIDATOS;i++)
+    {
+        match[i].media=0;
     }
-    
-    return j;//retorna quantos entradas foram colocadas na lista
+    for(i=0;i<nCursos;i++)
+    {
+        for(j=0;j<5;j++)
+        {
+            nSelecionados[i][j]=0;
+        }
+    }
+
+    j=0; //reaproveitamento do contador
+    for(i=0;i<nCandidatos;i++)
+    {
+        match[j].idCandidato=i;
+        match[j].idCurso=candidato[i].opcao1.idCurso;
+        match[j].media=candidato[i].opcao1.media;
+        match[j].opcao=1;
+        match[j].grupo=candidato[i].opcao1.grupo;
+        match[j].classificado=0;
+        j++;
+        printf("%s - Opcao 1/",candidato[i].nome);
+        if(candidato[i].opcao2.idCurso!=-1)
+        {
+            match[j].idCandidato=i;
+            match[j].idCurso=candidato[i].opcao2.idCurso;
+            match[j].media=candidato[i].opcao2.media;
+            match[j].opcao=2;
+            match[j].grupo=candidato[i].opcao2.grupo;
+            match[j].classificado=0;
+            j++;
+            printf("Opcao 2");
+        }
+        printf(" OK!\n");
+    }
+    return j;//retorna quantos foram colocados na lista
 }
