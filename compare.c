c8413d9290a0dbcbedcd2177a403816b9b7080e2
caioasilva <caiosilva96@hotmail.com>
2015-06-29 16:34:01 -0300

    Create compare.c

---
+++ compare.c
@@ -0,0 +1,10 @@
+int compare(tMatch *elem1, tMatch *elem2){
+   if ( elem1->media < elem2->media)
+      return 1;
+      
+   else if (elem1->media > elem2->media)
+      return -1;
+
+   else
+      return 0;
+}
