fa94a13949e60167e15d3b85dc62cd9ddbb3dada
brunazamith <628093@comp.ufscar.br>
2015-07-01 02:14:16 -0300

    Update ordenaLista.c

--- ordenaLista.c
+++ ordenaLista.c
@@ -1,3 +1,11 @@
+/*
+   Nome da função: ordenaLista
+   Objetivos: Ordenar a lista de escolhas de curso de acordo com a media
+   Parâmetros Formais:
+       -match[]: Vetor de escolha de cursos (Entrada e saída de dados)
+       -nMatch: Número de candidatos na lista (Entrada de dados)
+   Retorno: Nenhum (procedimento)
+*/
 void ordenaLista(tMatch match[],int nMatch){
     int i; //contador de laço
     printf("Ordenando Lista...\n");
