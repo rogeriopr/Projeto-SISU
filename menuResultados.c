6069fa958d0ab73cbead233e16f103095e4aeb72
caioasilva <caiosilva96@hotmail.com>
2015-07-01 01:57:04 -0300

    Update menuResultados.c

--- menuResultados.c
+++ menuResultados.c
@@ -4,27 +4,31 @@ void menuResultados(tMatch match[],int nMatch,tCandidato candidato[], int nCandi
 
 	do {
 		printf("Escolha a opcao:\n\n\t1. Exportar Aprovados Geral\n\t2. Exportar Aprovados por curso\n\t3. Exportar Aprovados por curso e grupo\n\t4. Busca por candidato\n\t5. Busca por curso\n\n\t0. Sair\n\nOpcao: ");
-		while (scanf("%d", &opcao) != 1);
+		opcao=getch();
 		switch (opcao) {
-		case 1:
+		case '1':
 			system("cls");
 			aprovadosTodos(match, nMatch, candidato, curso);	//Exibir a lista de aprovados ordenados por media (decrescente)
 			break;
-		case 2:
+		case '2':
 			system("cls");
 			aprovadosCurso(match, nMatch, candidato, curso, nCursos);	//Exibicao dos aprovados separados por cursos
 			break;
-		case 3:
+		case '3':
 			system("cls");
 			aprovadosGrupo(match, nMatch, candidato, curso, nCursos);	//Exibicao da lista de aprovados separados por cursos e grupos
 			break;
-		case 4:
+		case '4':
 			system("cls");
 			procuraCandidatos(candidato, nCandidatos, curso);	//Opcao para procurar candidatos pelo nome
 			break;
-        case 5:
+        	case '5':
 			system("cls");
 			procuraCurso(match, nMatch, candidato, curso, nCursos);	//Opcao para procurar candidatos pelo nome
+			break;
+		default:
+			system("cls");
+			printf("Opcao Invalida!");
 			break;
 		}
 		printf("\n\n");
