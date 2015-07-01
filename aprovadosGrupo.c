9d0cdbb980b899565c95f816eb8c0922dfeab792
caioasilva <caiosilva96@hotmail.com>
2015-07-01 09:33:41 -0300

    Update aprovadosGrupo.c

--- aprovadosGrupo.c
+++ aprovadosGrupo.c
@@ -27,18 +27,19 @@ void aprovadosGrupo(tMatch match[],int nMatch,tCandidato candidato[],tCurso curs
             return;
         }
 
-        printf("Classificados por curso e grupo:\n\n");
+        printf("Exportando dados para o arquivo...\n\n");
+        //printf("Classificados por curso e grupo:\n\n");
         fprintf(ifp,"Classificados por curso e grupo:\n\n");
 
         //Loop para preenchimento da lista de aprovados
         for(j=0; j < nCursos; j++){
 
-            printf("Curso: %s\n",curso[j].nome);
+            //printf("Curso: %s\n",curso[j].nome);
 
 
             for(c = 1; c <= N_GRUPOS; c++){
 
-                printf("Grupo %d:\n", c);
+                //printf("Grupo %d:\n", c);
                 fprintf(ifp,"Grupo %d:\n",c);
                 fprintf(ifp,"Curso:;%s\nPosicao;Nome;Opcao;Media\n",curso[j].nome);
 
@@ -48,15 +49,18 @@ void aprovadosGrupo(tMatch match[],int nMatch,tCandidato candidato[],tCurso curs
                 //Escrita dos alunos na lista de cada grupo
                 for(i=0;i<nMatch;i++){
                 if(match[i].classificado==1 && match[i].idCurso==j && match[i].grupo == c){
-                    printf("%3d. %10s Opcao: %d Media: %.2Lf\n", k, candidato[match[i].idCandidato].nome, match[i].opcao, match[i].media);
+                    //printf("%3d. %10s Opcao: %d Media: %.2Lf\n", k, candidato[match[i].idCandidato].nome, match[i].opcao, match[i].media);
                     fprintf(ifp,"%d;%s;%d;%.2Lf\n", k++, candidato[match[i].idCandidato].nome, match[i].opcao, match[i].media);
                 }
             }
 
-        printf("\n\n",curso[j].nome);
+        //printf("\n\n",curso[j].nome);
         fprintf(ifp,"\n\n",curso[j].nome);
         }
     }
+    //limpa a tela
+    system("cls");
+    printf("Dados exportados para o arquivo: %s\n",OUTPUT_CURSO_GRUPOS_FILE);
     fclose(ifp);
 
 }
