35df5088bbff5f26e5f328ad69a7f1bda3a696e0
Leonardo Tavares Oliveira <628174@comp.ufscar.br>
2015-07-01 02:21:53 -0300

    Update procuraCandidatos.c

--- procuraCandidatos.c
+++ procuraCandidatos.c
@@ -1,4 +1,17 @@
 /*
+   Nome da função: toUpperStr
+   Objetivos: coloca uma string em upper case
+   Parâmetros Formais:
+       -str: String para ser colocada em upper case(Entrada e saida de dados)
+   Retorno: Nenhum (procedimento)
+*/
+void toUpperStr(char *str){
+    int i;
+
+    for(i = 0; i < strlen(str);i++)
+        str[i]=toupper(str[i]);
+}
+/*
    Nome da função: procuraCandidatos
    Objetivos: imprime os resultados de um candidato especifico
    Parâmetros Formais:
@@ -15,19 +28,23 @@ void procuraCandidatos(tCandidato candidato[],int nCandidatos,tCurso curso[])
     printf("Nome do Candidato: ");
     fflush(stdin);
     gets(nome);
+    toUpperStr(nome);
+
+    printf("\n-----------\n");
 
     for(i=0;i<nCandidatos;i++) {
+        toUpperStr(candidato[i].nome);
         if(strstr(candidato[i].nome,nome)!=NULL) { // Compara o nome do teclado com os nomes dos candidatos dentro do vetor candidato.
 					        // Requer a biblioteca <string.h>.
             k++;
             printf("ID: %d\nNome: %s\n",i,candidato[i].nome);
-            printf("1. Opcao:\nCurso: %s\nGrupo: %d\nNota: %.2Lf\n",curso[candidato[i].opcao1.idCurso].nome,candidato[i].opcao1.grupo,candidato[i].opcao1.media);
+            printf("1. Opcao:\n\tCurso: %s\n\tGrupo: %d\n\tNota: %.2Lf\n",curso[candidato[i].opcao1.idCurso].nome,candidato[i].opcao1.grupo,candidato[i].opcao1.media);
             if(candidato[i].classificado == -2) {
                 printf("Candidato aprovado na 1. Opcao\n");
             }else{
-                printf("Candidato não aprovado na 1. Opcao\n");
+                printf("Candidato nao aprovado na 1. Opcao\n");
             }
-            printf("\n2. Opcao:\nCurso: %s\nGrupo: %d\nNota: %.2Lf\n",curso[candidato[i].opcao2.idCurso].nome,candidato[i].opcao2.grupo,candidato[i].opcao2.media);
+            printf("\n2. Opcao:\n\tCurso: %s\n\tGrupo: %d\n\tNota: %.2Lf\n",curso[candidato[i].opcao2.idCurso].nome,candidato[i].opcao2.grupo,candidato[i].opcao2.media);
             if(candidato[i].classificado >=0) {
                 printf("Candidato aprovado na 2. Opcao\n");
             }
@@ -35,7 +52,7 @@ void procuraCandidatos(tCandidato candidato[],int nCandidatos,tCurso curso[])
             {
                 printf("Candidato nao aprovado na 2. Opcao\n");
             }
-            printf("\n");
+            printf("-----------\n");
         }
     }
 
