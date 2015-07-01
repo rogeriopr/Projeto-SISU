c6ed92163ef8b2c19aa1e8ecfc6fecf7787b0cb7
caioasilva <caiosilva96@hotmail.com>
2015-07-01 09:16:48 -0300

    Update procuraCurso.c

--- procuraCurso.c
+++ procuraCurso.c
@@ -13,27 +13,31 @@ void procuraCurso(tMatch match[],int nMatch,tCandidato candidato[],tCurso curso[
 {
     //declaracao das variaveis
     char nome[TAM_NOME];
-    int i,j,k=0;
+    int i,j,k;
 
     //leitura do nome do curso
     printf("Nome do Curso: ");
     fflush(stdin);
-    gets(nome);
+    while(strlen(gets(nome))<3)
+    {
+        system("cls");
+        printf("Digite pelo menos 3 caracteres!\n\nNome do Curso: ");
+    }
     toUpperStr(nome);
 
+    system("cls");
     //pesquisa para cursos com nome parecido
     for(i=0;i<nCursos;i++) {
 
         toUpperStr(curso[i].nome);
         if(strstr(curso[i].nome,nome)!=NULL) { // Compara o nome do teclado com os nomes dos candidatos dentro do vetor candidato.
 					        // Requer a biblioteca <string.h>.
-            k++;
-
+            k=1;
             //impressao dos alunos aprovados
             printf("-%s-\n\n",curso[i].nome);
             for(j=0; j<nMatch; j++){
                 if(match[j].classificado==1 && match[j].idCurso==i){
-                    printf("\t%s %*s %d Grupo: %d Media: %.2Lf\n", candidato[match[j].idCandidato].nome,abs(35-strlen(candidato[match[j].idCandidato].nome)) , "Opcao:" , match[j].opcao, match[j].grupo, match[j].media);
+                    printf("\t%3d. %s %*s %d Grupo: %d Media: %.2Lf\n", k++, candidato[match[j].idCandidato].nome,abs(35-strlen(candidato[match[j].idCandidato].nome)) , "Opcao:" , match[j].opcao, match[j].grupo, match[j].media);
                 }
             }
 
