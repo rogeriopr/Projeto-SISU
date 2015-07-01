eb43e32daf5fc64ce8c45f837f851c62a6e48fed
brunazamith <628093@comp.ufscar.br>
2015-07-01 02:11:10 -0300

    Update lerCurso.c

--- lerCurso.c
+++ lerCurso.c
@@ -1,42 +1,38 @@
-//
-//  Arquivo: lerCurso.c
-//  Autor: Marco Aurélio Murbach Teles Machado R.A. 418927
-//  Data: 29 de Junho de 2015
-//  Objetivos: Função para ler arquivo com os dados dos cursos, retornando o total de cursos lidos e o vetor
-//  tCurso com os dados já cadastrados.
-//
-//
-
-
-// Objetivos: Leitura do arquvio CURSOS_FILE, com os dados do curso
-// Parametros: tCurso curso (Entrada e Saída)
-// Retorno: nCursos -> Total de Cursos Cadastrado
+/*
+   Nome da função: lerCursos
+   Objetivos: Leitura do arquvio CURSOS_FILE, com os dados do curso
+   Parâmetros Formais:
+       -curso[]: Vetor de tCurso (Entrada e saída de dados)
+   Retorno: Número de cursos lidos (int nCursos)
+*/
 int lerCursos(tCurso curso[])
 {
     FILE *ifp; //Identificação do Arquivo
     char *mode = "r"; //Modo Leitura (Somente)
-    int i,nCursos,j;
+    int i,nCursos,j; //Declaração de Variáveis
     char nomeTemp[TAM_NOME];
-    
+
     ifp = fopen(CURSOS_FILE, mode);
-    
-    //--> Caso não encontre o Arquivo para o programa
+
+    // Caso não encontre o Arquivo para o programa
     if (ifp == NULL) {
         printf("Erro na abertura do arquivo %s\n",CURSOS_FILE);
         system("pause");
         exit(1);
     }
-    
-    //--> Recupera a Quantidade de Cursos
+
+    // Recupera a Quantidade de Cursos
     fscanf(ifp, "%d", &nCursos);
-    printf("Lendo %d cursos\n",nCursos);
-    
+
     // Inicia a leitura do Arquivo de Cursos
+    printf("Lendo lista de cursos.\n");
+    printf("Lendo lista de cursos..\n");
+    printf("Lendo lista de cursos...\n");
     for(i=0;i<nCursos;i++){
-        
-        // --> Leitura do Nome do Curso
+
+        //Leitura do Nome do Curso
         fscanf(ifp,"%s",nomeTemp);
-        
+
         //correcao do nome
         for( j = 0;j < strlen(nomeTemp); j++){
             if(nomeTemp[j]==95){
@@ -44,26 +40,24 @@ int lerCursos(tCurso curso[])
             }
         }
         strcpy(curso[i].nome,nomeTemp);
-        
-        //--> Após Ler o Nome do Curso recupera a quantidade de Vagas de Cada Grupo
+
+        // Após Ler o Nome do Curso recupera a quantidade de Vagas de Cada Grupo
         fscanf(ifp, "%d %d %d %d %d", &curso[i].vagas[1],&curso[i].vagas[2],&curso[i].vagas[3],&curso[i].vagas[4],&curso[i].vagas[5]);
-        
-        //--> Vaga[0] Grava total de vagas oferecida pelo curso
+
+        // Vaga[0] Grava total de vagas oferecida pelo curso
         curso[i].vagas[0] = curso[i].vagas[1]+curso[i].vagas[2]+curso[i].vagas[3]+curso[i].vagas[4]+curso[i].vagas[5];
-        
-        //--> Le os Pesos
+
+        // Lê os Pesos
         fscanf(ifp, "%lf %lf %lf %lf %lf", &curso[i].pesos.humanas,&curso[i].pesos.linguagens,&curso[i].pesos.matematica,&curso[i].pesos.natureza,&curso[i].pesos.redacao);
-        
-        //--> Imprime os Nomes dos Cursos Lidos
-        printf("Curso[%d]: %s\n", i, curso[i].nome);
+
     }
-    
-    //--> Informa o Sucesso ao importar todos os cursos
-    printf("Sucesso!\n\n");
-    
-    //--> Fecha o Arquivo
+
+    // Informa o Sucesso ao importar todos os cursos
+    printf("Sucesso ao importar cursos!\n\n");
+
+    // Fecha o Arquivo
     fclose(ifp);
-    
-    //--> Retorna Quantidade de Curso Lida
+
+    // Retorna Quantidade de Curso Lida
     return nCursos;
 }
